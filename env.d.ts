2a358c723fee2a4167cac9fc5f218f6b809f1e97
Sushaan Kandukoori <sushaankandukoori@gmail.com>
2025-04-21 21:03:23 -0400

    Modify Cloudflare configuration files instead of removing them

---
+++ env.d.ts
@@ -0,0 +1,4 @@
+// This file has been modified to remove Cloudflare dependencies for Vercel deployment
+
+// Empty interface to avoid TypeScript errors
+interface CloudflareEnv {}
---
+++ open-next.config.ts
@@ -0,0 +1,7 @@
+// This file has been modified to remove Cloudflare dependencies for Vercel deployment
+
+const config = {
+  // Configuration removed to enable Vercel deployment
+};
+
+export default config;
